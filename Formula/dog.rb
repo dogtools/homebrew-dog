7e051bd29e925f635eeb0ac8d178a9952742d396
Xavi Soler <xsb@interrupciones.net>
2016-08-12 14:48:01 +0200

    Bump version to v0.2.0

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -1,7 +1,7 @@
 class Dog < Formula
   desc "Command-line application that executes tasks"
   homepage "https://github.com/dogtools/dog"
-  url "https://github.com/dogtools/dog/releases/download/v0.1.0/dog-v0.1.0.tar.gz"
+  url "https://github.com/dogtools/dog/releases/download/v0.2.0/dog-v0.2.0.tar.gz"
   sha256 "f584e9d074b5d08a3e2cd5707cfa8961cc3dfa27102a78cc287f86dd061cd2fe"
 
   def install
