de3224b0430f19ef10b9da7dbf9bc7fb5c87d39e
Marc Guasch <marc-gr@users.noreply.github.com>
2016-08-12 14:51:28 +0200

    Update dog.rb

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -2,7 +2,7 @@ class Dog < Formula
   desc "Command-line application that executes tasks"
   homepage "https://github.com/dogtools/dog"
   url "https://github.com/dogtools/dog/releases/download/v0.2.0/dog-v0.2.0.tar.gz"
-  sha256 "f584e9d074b5d08a3e2cd5707cfa8961cc3dfa27102a78cc287f86dd061cd2fe"
+  sha256 "d28969a50b504c24f00666e99dc89d467195327c412aa7aa698a60f2f6a7d238"
 
   def install
     bin.install "dist/darwin_amd64/dog"
