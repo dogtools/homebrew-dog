8f3086b8a1da1b4dafc685e2bf70cde29d457891
Marc Guasch <marc@typeform.com>
2016-08-12 12:11:11 +0200

    Add formular

---
+++ Formula/dog.rb
@@ -0,0 +1,10 @@
+class Dog < Formula
+  desc "Command-line application that executes tasks"
+  homepage "https://github.com/dogtools/dog"
+  url "https://github.com/dogtools/dog/releases/download/v0.1.0/dog-v0.1.0.tar.gz"
+  sha256 "f584e9d074b5d08a3e2cd5707cfa8961cc3dfa27102a78cc287f86dd061cd2fe"
+
+  def install
+    prefix.install "darwin_amd64/dog"
+  end
+end
