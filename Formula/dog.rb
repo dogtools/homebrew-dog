cbaa8ae179d276538c88271f0afddb67041b5601
Xavi Soler <xavi@typeform.com>
2018-05-18 16:58:28 +0200

    Change file extension to zip

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -1,7 +1,7 @@
 class Dog < Formula
   desc "Dog is a command line application that executes automated tasks"
   homepage "https://github.com/dogtools/dog"
-  url "https://github.com/dogtools/dog/releases/download/v0.5.0/dog-v0.5.0.tar.gz"
+  url "https://github.com/dogtools/dog/releases/download/v0.5.0/dog-v0.5.0.zip"
   sha256 "44302b195e00124d26d867b707bb3b4559823083cd2cecddb4e7c71e0025caae"
 
   def install
